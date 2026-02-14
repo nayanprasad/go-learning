34a313b799ccfd33a3828949e8f43e46bddf74d6
nayanprasad <nayanprasad096@gmail.com>
2026-02-14 15:30:16 +0530

    feat: configured db connection

--- api-ecom/cmd/main.go
+++ api-ecom/cmd/main.go
@@ -1,27 +1,44 @@
 package main
 
 import (
+	"api-ecom/internal/env"
+	"context"
 	"log/slog"
 	"os"
+
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
-	cnf := config{
-		addr: ":5055",
-		db:   dbConfig{},
-	}
-
-	api := appplication{
-		config: cnf,
-	}
 
 	//logger
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
 
-	h := api.mount()
-	err := api.run(h)
+	ctx := context.Background()
+
+	cnf := config{
+		addr: ":5055",
+		db: dbConfig{
+			dsn: env.Get("GOOSE_DBSTRING", ""),
+		},
+	}
+
+	conn, err := pgx.Connect(ctx, env.Get("GOOSE_DBSTRING", "host=localhost user=postgres password=postgres dbname=api-ecom-go sslmode=disable"))
 	if err != nil {
+		slog.Error("Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(ctx)
+
+	slog.Info("connected to database", "dsn", cnf.db.dsn)
+
+	api := appplication{
+		config: cnf,
+	}
+
+	h := api.mount()
+	if err := api.run(h); err != nil {
 		slog.Error("Error starting the server", "error", err)
 		os.Exit(1)
 	}
